2dce0565e4e6a7fc0b50508ba34028084265f4a6
lakers31185 <lakers31185@gmail.com>
2014-07-02 01:41:42 -0700

    update

--- Homework/Assignment 2/Gaddis_7thEd_Chap3_Prob5/main.cpp
+++ Homework/Assignment 2/Gaddis_7thEd_Chap3_Prob5/main.cpp
@@ -23,19 +23,19 @@ int main(int argc, char** argv) {
 //Declare variables
 
     //Inputs
-    string movie;
-    unsigned short adlTik,
-                   kidTik;
-    float aldPri= 6.00, 
-          kidPri= 3.00,
-          boxPer= .20f,
-          grosTot;
+    string movie;           //Movie Selected From Box Office
+    unsigned short adlTik,  //Enter Total Number of Adult Tickets
+                   kidTik;  //Enter Total Number of Kids Tickets
+    float aldPri= 6.00,     //Price of Adult Tickets
+          kidPri= 3.00,     //Price of Kids Tickets
+          boxPer= .20f,     //Percentage Theater keeps for Profit 
+          grosTot;          //Gross Box Office Profit
    
-    //Outputs
-    unsigned short adlTot,
-                   kidTot;
-    float netPro,
-          amtPaid;
+    //Outputs   
+    unsigned short adlTot,  //Total Number Adult Tickets Purchased
+                   kidTot;  //Total Number Kids Tickets Purchased
+    float netPro,           //Net Box Office Profit
+          amtPaid;          //Amount Paid to Distributor
     
     //Enter Name of Movie
     cout<<"Enter the Name of Movie Your Going to Watch?"<<endl;
