a410ad8f5989253fedbd789f12fb4b860360b11b
lakers31185 <lakers31185@gmail.com>
2014-07-04 01:39:41 -0700

    pro5

---
+++ Homework/Assignment 3/Gaddis_7thEd_Chap4_Prob5_bodMass/main.cpp
@@ -0,0 +1,57 @@
+/* 
+ * File:   main.cpp
+ * Author: Tony Reyes
+ * July, 3 2014
+ * Purpose: Gaddis 7thEd Chap4 Prob5 Body Mass Index
+ */
+
+//System Libraries
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+//User Libraries
+
+
+//Global Constants
+
+
+//Function Prototypes
+
+
+//Execution Starts Here!
+int main(int argc, char** argv) {
+
+
+//Declare variables & Initialize Entries
+
+    //Inputs
+    int weigh,
+        heigh,
+            bmiMin= 18.5,
+            bmiMax= 25;
+    
+    //Outputs
+    float bMi=0;
+    
+    //Enter Weight and Height
+    cout<<"Enter Your Height "
+    cin>>heigh;
+    cout << "Enter Your Weight "
+    cin>>weigh;
+    
+//Calculations
+    
+    //Calculate Body Mass Index
+    bMi= (weigh*703)/(heigh*heigh);   
+//Validate Outputs
+     
+    //Validate BMI
+    if (bMi > bmiMin) && (bmiMax < bMi)
+        cout<<"Weight is OPtimal"
+    else if 
+    
+    //Exit stage right!   
+    return 0;
+}
