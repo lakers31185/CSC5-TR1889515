170f221cd0709ecdfc244184077cf2b154e74b4e
lakers31185 <lakers31185@gmail.com>
2014-07-05 13:07:21 -0700

    prob 6

---
+++ Homework/Assignment 3/Gaddis_7thEd_Chap4_Prob6_masWeig/main.cpp
@@ -0,0 +1,62 @@
+/* 
+ * File:   main.cpp
+ * Author: Tony Reyes
+ * July, 2 2014
+ * Purpose: Gaddis 7thEd Chap4 Prob6 Mass & Weight
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+
+//User Libraries
+
+
+//Global Constants
+
+
+//Function Prototypes
+
+
+//Execution Starts Here!
+int main(int argc, char** argv) {
+
+    //Declare variables & Initialize Entries
+
+    //Inputs
+    float mass;
+        
+    //Outputs
+   float weigh;
+    
+    //Enter Mass of Object to be Calculated
+    cout<<"Enter Objects Math: ";
+    cin>>mass;
+    
+//Calculations 
+
+    //Calculate using Newton Formula & Display
+    weigh= mass*9.8e-1f;
+    cout<<"Calculated Weight= " <<weigh<<endl;
+    
+//Validate Outputs
+    
+    //Set Ranges $ Validate Expressions
+    if (weigh < 10.0) 
+       cout<<"Object Too Light";
+    else if (weigh > 1000.0)
+          cout<<"Object Too Heavy ";
+    else if (weigh >= 10.0 < 1000.0)
+        cout<<"Wait is Normal";            
+  
+//Exit stage right!   
+    return 0;
+} 
+
+ 
+ 
+
+
+ 
+   
