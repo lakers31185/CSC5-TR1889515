cb8ea123c8e2fe8fdc002ab797a0201311c06846
lakers31185 <lakers31185@gmail.com>
2014-07-22 13:04:31 -0700

    prob 1

--- Homework/Assignment 5/Gaddis_Chap6_Prob1_mrkUp/main.cpp
+++ Homework/Assignment 5/Gaddis_Chap6_Prob1_mrkUp/main.cpp
@@ -23,43 +23,48 @@ using namespace std;
 //Function Prototypes
  float calReta (float,float);
 
-//Execution Starts Here!
-int main(int argc, char** argv){
+    //Execution Starts Here!
+    int main(int argc, char** argv){
     //Declare Variables
     //Inputs
-    float mrkUp,
-         percnt,
-            total,
-            price;
-    
-   do{
+    float mrkUp,            //User Enters Wholesale Cost
+    percnt,            //User Enters Percent Mark Up
+    total;            //Calculated Mark Up Price 
+
+    //Validate Wholesale Cost is Positive Number
+    do{
         cout<<"What is WholeSale Cost?: "<<endl;
         cin>>mrkUp;
-            if (mrkUp < 1)
-            cout<<"Enter Positive Number! "<<endl;
+        if (mrkUp < 1)
+        cout<<"Enter Positive Number! "<<endl;
     }while(!(mrkUp>0&&mrkUp<1e9));
-    
-    do{
-         cout<<"What is Percent Mark Up?: "<<endl;
-        cin>>percnt;
-            if (percnt < 1)
-            cout<<"Enter Positive Number! "<<endl;
-    }while(!(percnt>0&&percnt<1e9));
-    
-   
 
-    
+    //Validate Percent Mark Up is Positive Number
+    do{
+        cout<<"What is Percent Mark Up?: "<<endl;
+        cin>>percnt;
+        if (percnt < 1)
+        cout<<"Enter Positive Number! "<<endl;
+    }while(!(percnt>0&&percnt<1e9));
+
+    //Set Precision for Dollar Amount 
     cout<<setprecision(2)<<fixed;
+
+    //Calculate Conversion to Find Mark Up Amount
     total= calReta(percnt,mrkUp);
 
-   
-   
+    //Display Output Amount of Mark Up
     cout<<"The Mark Up Price Is:($) "<<total<<endl;
-return 0;
+    return 0;
 }
+//Price Mark Up Function
+//Formula Used Equate Amount
+//cost= Percent  mrk=Mark  
 float calReta(float cost,float mrk)
 {
-    
-    return (cost/100*mrk)+mrk;
-    
+//Price Mark Up Function
+//Formula Used Equate Amount
+//cost= Percent  mrk=Mark Up
+return (cost/100*mrk)+mrk;
+
 }
