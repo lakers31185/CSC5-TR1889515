98552e66bd8bf8bcc22dacdf6c075c791f80247c
lakers31185 <lakers31185@gmail.com>
2014-07-24 11:49:48 -0700

    update

--- Homework/Assignment 5/Gaddis_Chap6_Prob1_mrkUp/main.cpp
+++ Homework/Assignment 5/Gaddis_Chap6_Prob1_mrkUp/main.cpp
@@ -19,32 +19,20 @@ using namespace std;
 
 
 //Function Prototypes
- float calReta (float,float);
+void mrkPer(float &mrkUp,float &percnt); 
+float calReta (float,float);
 
     //Execution Starts Here!
     int main(int argc, char** argv){
     //Declare Variables
     //Inputs
-    float mrkUp,            //User Enters Wholesale Cost
+    float mrkUp,        //User Enters Wholesale Cost
     percnt,            //User Enters Percent Mark Up
     total;            //Calculated Mark Up Price 
 
-    //Validate Wholesale Cost is Positive Number
-    do{
-        cout<<"What is WholeSale Cost?: "<<endl;
-        cin>>mrkUp;
-        if (mrkUp < 1)
-        cout<<"Enter Positive Number! "<<endl;
-    }while(!(mrkUp>0&&mrkUp<1e9));
-
-    //Validate Percent Mark Up is Positive Number
-    do{
-        cout<<"What is Percent Mark Up?: "<<endl;
-        cin>>percnt;
-        if (percnt < 1)
-        cout<<"Enter Positive Number! "<<endl;
-    }while(!(percnt>0&&percnt<1e9));
-
+    //Call Reference Function for Percent & Mark Up
+    mrkPer(percnt,mrkUp);
+    
     //Set Precision for Dollar Amount 
     cout<<setprecision(2)<<fixed;
 
@@ -55,12 +43,32 @@ using namespace std;
     cout<<"The Mark Up Price Is:($) "<<total<<endl;
     return 0;
 }
+//Reference Function of Values for Wholesale Cost &  Mark Up Percent
+//&mrkUP = Whole CSale Cost  &percent = Percent Mark Up
+void mrkPer(float &mrkUp,float &percnt)
+{
+//Validate Wholesale Cost is Positive Number
+    do{
+    cout<<"What is WholeSale Cost?: "<<endl;
+    cin>>mrkUp;
+    if (mrkUp < 1)
+        cout<<"Enter Positive Number! "<<endl;
+}while(!(mrkUp>0&&mrkUp<1e9));
+
+//Validate Percent Mark Up is Positive Number
+do{
+    cout<<"What is Percent Mark Up?: "<<endl;
+    cin>>percnt;
+    if (percnt < 1)
+    cout<<"Enter Positive Number! "<<endl;
+}while(!(percnt>0&&percnt<1e9));
+}
 //Price Mark Up Function
 //Formula Used Equate Amount
 //cost= Percent  mrk=Mark  
 float calReta(float cost,float mrk)
 {
-//Return Value
+//Return Mark up Value
 return (cost/100*mrk)+mrk;
 
 }
