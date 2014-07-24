08582339a1e4faad56b6fbba172df667439dc60b
lakers31185 <lakers31185@gmail.com>
2014-07-24 11:59:20 -0700

    up

--- Homework/Chapter 6 menu/main.cpp
+++ Homework/Chapter 6 menu/main.cpp
@@ -76,40 +76,27 @@ int getN(){
 //Solution to problem 1
 void problem1(){
             //Function Prototypes
-    float calReta (float,float);
-    //Declare Variables
+void mrkPer(float &mrkUp,float &percnt); 
+float calReta (float,float);
+   //Declare Variables
     //Inputs
-    float mrkUp,            //User Enters Wholesale Cost
+    float mrkUp,        //User Enters Wholesale Cost
     percnt,            //User Enters Percent Mark Up
     total;            //Calculated Mark Up Price 
 
-    //Validate Wholesale Cost is Positive Number
-    do{
-        cout<<"What is WholeSale Cost?: "<<endl;
-        cin>>mrkUp;
-        if (mrkUp < 1)
-        cout<<"Enter Positive Number! "<<endl;
-    }while(!(mrkUp>0&&mrkUp<1e9));
-
-    //Validate Percent Mark Up is Positive Number
-    do{
-        cout<<"What is Percent Mark Up?: "<<endl;
-        cin>>percnt;
-        if (percnt < 1)
-        cout<<"Enter Positive Number! "<<endl;
-    }while(!(percnt>0&&percnt<1e9));
-
+    //Call Reference Function for Percent & Mark Up
+    mrkPer(percnt,mrkUp);
+    
     //Set Precision for Dollar Amount 
     cout<<setprecision(2)<<fixed;
 
-    //Calculate Conversion Function to Find Mark Up Amount
+    //Calculate Conversion to Find Mark Up Amount
     total= calReta(percnt,mrkUp);
 
     //Display Output Amount of Mark Up
     cout<<"The Mark Up Price Is:($) "<<total<<endl;
-   
-
-            }
+ 
+}
 //Solution to problem 2
 void problem2(){
     //Function Prototypes
@@ -344,6 +331,26 @@ int calclAve(int, int, int, int, int,int);
 //Exit Comment
 void def(int inN){
     cout<<"You typed "<<inN<<" to exit the program"<<endl;
+}
+//Reference Function of Values for Wholesale Cost &  Mark Up Percent
+//&mrkUP = Whole CSale Cost  &percent = Percent Mark Up
+void mrkPer(float &mrkUp,float &percnt)
+{
+//Validate Wholesale Cost is Positive Number
+    do{
+    cout<<"What is WholeSale Cost?: "<<endl;
+    cin>>mrkUp;
+    if (mrkUp < 1)
+        cout<<"Enter Positive Number! "<<endl;
+}while(!(mrkUp>0&&mrkUp<1e9));
+
+//Validate Percent Mark Up is Positive Number
+do{
+    cout<<"What is Percent Mark Up?: "<<endl;
+    cin>>percnt;
+    if (percnt < 1)
+    cout<<"Enter Positive Number! "<<endl;
+}while(!(percnt>0&&percnt<1e9));
 }
 //Price Mark Up Function
 //Formula Used Equate Amount
